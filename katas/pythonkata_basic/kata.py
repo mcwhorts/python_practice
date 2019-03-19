2ca85c7140c9a01b68a8e27fb49af6171d856754
Scott McWhorter <smcwhorter@nuskin.com>
2019-03-19 13:16:48 -0600

    syncing

--- katas/pythonkata_basic/kata.py
+++ katas/pythonkata_basic/kata.py
@@ -5,6 +5,12 @@
 3. Print both integers
 4. Type cast a integer variable from 1 or 2 into a float and print it.
 """
+dog = 6
+cat = 9
+print(dog)
+print(cat)
+print(float(cat))
+
 
 rock = 12
 stone = 7
@@ -21,6 +27,17 @@ print(float(rock))
 6. float()
 7. len()
 """
+
+
+type(cat)
+print(dog)
+print(list(range(0,10)))
+int(12.675)
+str('kangaroo')
+float(67)
+len(["rabbits","cows","chickens","robots"])
+
+
 print(type("cats")) # print type string
 print(list(range(1,100,2))) # Print every other number from 1-100
 int_as_string = str(15)
@@ -38,13 +55,25 @@ print(len([1,4,19]))
 ```
 numlist = list(range(10))
 ```
-6. 0utput only the first 3 elements of numlist
-7. output only the last 3 elements of numlist
-8. output every other element of numlist
-9. output numlist backwards
-10.  Loop through numlist and print every element
+6. Output only the first 3 elements of numlist
+7. Output only the last 3 elements of numlist
+8. Output every other element of numlist
+9. Output numlist backwards
+10. Loop through numlist and print every element
 """
-
+multi_type_list = ['salmon',62,78.32]   #Define a list with 1 string, 1 int and 1 float
+print(multi_type_list[2])   #Print the 3rd element of your list
+empty_list = []             #Define a emply list
+multi_type_list.reverse()   #Step 4
+print(multi_type_list)      #Step 4
+multi_type_list[::-1]       #Step 4
+print(multi_type_list)      #Step 4
+numlist = list(range(10))   #Create a list of numbers from 0-9
+print(numlist)              #Create a list of numbers from 0-9
+print(numlist[:3])          #Output only the first 3 elements of numlist
+print(numlist[-3:])         #Output only the last 3 elements of numlist
+print(numlist[::2])         #Output every other element of numlist
+print(numlist[::-1])        #Output numlist backwards
 
 
 ### Dict Challenges
